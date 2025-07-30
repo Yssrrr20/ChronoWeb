6067cbfbc6f2a0c67a70977370694cd6ddd1a0bc
Yssrrr20 <muhyassar20@gmail.com>
2025-07-30 09:48:02 +0700

    Update cloudinaryConfig.js

--- Frontend/src/lib/backend/config/cloudinaryConfig.js
+++ Frontend/src/lib/backend/config/cloudinaryConfig.js
@@ -3,9 +3,9 @@ import { v2 as cloudinary } from 'cloudinary';
 
 // Konfigurasi Cloudinary
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME || 'dkchjb0st', 
-  api_key: process.env.CLOUDINARY_API_KEY || '375375945785333',           
-  api_secret: process.env.CLOUDINARY_API_SECRET || 'd08kNujb1Utaa6eB5nB8M_jwMLY'   
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME || 
+  api_key: process.env.CLOUDINARY_API_KEY ||           
+  api_secret: process.env.CLOUDINARY_API_SECRET ||    
 });
 
 export default cloudinary;
